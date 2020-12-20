ef9481fb480c3c9e6a0f785d7e8ac5347e44509c
ZhangYuke1220 <sonodateitoku@gmail.com>
2020-12-20 16:14:18 +0900

    uploaded add and free

--- initial.h
+++ initial.h
@@ -17,5 +17,7 @@ typedef struct
 void print_sample_set(SampleSet *data);
 int comp_alt(const void *x0, const void *x1);
 SampleSet *load_sample_set(const char *filename);
+void free_sample_set(SampleSet *data);
+SampleSet *add_sample(SampleSet *data, char *loc, double alt, double temp);
 
 #endif

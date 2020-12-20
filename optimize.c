c4d3877cd8e2720aafc4f6d16993f40865d21210
ZhangYuke1220 <sonodateitoku@gmail.com>
2020-12-20 15:20:50 +0900

    updated initial

--- optimize.c
+++ optimize.c
@@ -1,7 +1,8 @@
-#include <math.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include "optimize.h"
+# include <math.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include "optimize.h"
+# include "initial.h"
 
 double calc_norm(const int dim, double v[])
 {
@@ -14,39 +15,24 @@ double calc_norm(const int dim, double v[])
     return tmp;
 }
 
-int optimize(const double alpha, const int dim, double x[],
-             void (*calc_grad)(const double[], double[]))
+int optimize(const double alpha, const int dim, double x[], SampleSet *data,
+             void (*calc_grad)(const double[], double[], SampleSet *))
 {
-    // 勾配ベクトルを記録する領域を確保
     double *g = malloc(dim * sizeof(double));
-
     int iter = 0;
+
     while (++iter < 10000)
     {
-
-        // 引数で渡された関数を使って勾配ベクトルを計算
-        (*calc_grad)(x, g);
-
-        // 勾配ベクトルのノルムを評価
+        (*calc_grad)(x, g, data);
         const double norm = calc_norm(dim, g);
-        printf("%3d norm = %7.4f", iter, norm);
-        for (int i = 0; i < dim; i++)
-        {
-            printf(", x[%d] = %7.4f", i, x[i]);
-        }
-        printf("\n");
 
-        if (norm < 0.001)
+        if (norm < 0.0001)
             break;
 
-        // 最急降下法による更新
         for (int i = 0; i < dim; i++)
-        {
             x[i] -= alpha * g[i];
-        }
     }
 
     free(g);
-
     return iter;
 }

3cd6cb1f91e9c871996df944527ea46d74afeef9
ZhangYuke1220 <sonodateitoku@gmail.com>
2020-12-20 15:18:55 +0900

    included struct defination

---
+++ initial.h
@@ -0,0 +1,21 @@
+#ifndef OPTIMIZE_INIT_H
+#define OPTIMIZE_INIT_H
+
+typedef struct
+{
+    char *loc;   // location name
+    double alt;  // altitude (km)
+    double temp; // temperature (centigrade)
+} Sample;
+
+typedef struct
+{
+    unsigned int number;
+    Sample *sample;
+} SampleSet;
+
+void print_sample_set(SampleSet *data);
+int comp_alt(const void *x0, const void *x1);
+SampleSet *load_sample_set(const char *filename);
+
+#endif

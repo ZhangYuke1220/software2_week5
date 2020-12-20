8d6aa61e067c233f5750ebc0ea4ed2e49f3efe44
ZhangYuke1220 <sonodateitoku@gmail.com>
2020-12-20 15:23:16 +0900

    included initial

--- optimize.h
+++ optimize.h
@@ -1,7 +1,10 @@
-#ifndef OPTIMIZE_OPTIMIZE_H
-#define OPTIMIZE_OPTIMIZE_H
+# ifndef OPTIMIZE_OPTIMIZE_H
+# define OPTIMIZE_OPTIMIZE_H
+# include "initial.h"
+# include "func1.h"
 
-int optimize(const double alpha, const int dim, double x[], 
-	     void (*calc_grad)(const double [], double []));
+
+int optimize(const double alpha, const int dim, double x[], SampleSet *data,
+             void (*calc_grad)(const double[], double[], SampleSet *));
 
 #endif
